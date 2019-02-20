32d1c99ab7f4baa3a19606c3a66be1b9cf107f18
Alex Boag-Munroe <ninpo@qap.la>
2019-02-20 01:15:12 +0000

    Remove unneeded module

--- src/trio_inotify/sys_helpers.py
+++
@@ -1,9 +0,0 @@
-import array
-import fcntl
-from trio_inotify._ioctl_c import ffi, lib
-
-
-def get_fd_buffer_length(inotify_fd):
-    buffer = array.array("I", [0])
-    fcntl.ioctl(inotify_fd, lib.FIONREAD, buffer)
-    return buffer[0]
