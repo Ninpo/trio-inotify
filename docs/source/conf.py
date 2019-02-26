e458798be6acc2347f8bdd8bbb170223d13a3c7a
Alex Boag-Munroe <ninpo@qap.la>
2019-02-26 21:39:36 +0000

    Documentation fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 import re
-sys.path.insert(0, os.path.abspath('../src/trio_inotify/'))
+sys.path.insert(0, os.path.abspath('../src/'))
 
 
 # -- Project information -----------------------------------------------------
